7ddac18d6b2f43dd01912c9aec5af52846f37036
vizantija037 <80783335+vizantija037@users.noreply.github.com>
2022-02-20 15:41:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+const form = document.getElementById("formLogin");
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  let userFront = document.getElementById("username").value;
+  let passFront = document.getElementById("password").value;
+  loaderVisible();
+  setTimeout(loaderInvisible, 500)
+  fetch("https://fakestoreapi.com/users")
+    .then((resp) => resp.json())
+    .then((data) => {
+ 
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].username === userFront && data[i].password === passFront) {
+          NewTab();
+          resetLabel
+          form.reset();
+          
+        } else {
+          document.querySelector(".user-message").style.display = "flex";
+        }
+        
+        
+      }
+    })
+ 
+    
+}
+
+function NewTab() {
+  window.open("https://vizantija037.github.io/JSON-reading/", "_self");
+}
+
+form.addEventListener('click', resetLabel);
+function resetLabel(e){
+  if (e.target.type === 'text'){
+    document.querySelector(".user-message").style.display = "none";
+  }
+}
+
+const loader = document.querySelector('.loader-holder');
+
+function loaderVisible(){
+    loader.style.display = 'flex';
+}
+function loaderInvisible() {
+  loader.style.display = 'none';
+}
+
+
+
+
+fetch('https://fakestoreapi.com/users')
+    .then(response=>response.json())
+    .then(data=>{
+        
+        let output = '';
+        data.forEach(element => {
+                 
+            output += `
+            <div class="card">
+            <h4>Username: ${element.username}</h4>
+            <h4>Password: ${element.password}</h4>
+            </div>
+        `}
+        ) 
+          
+        document.querySelector('.users').innerHTML = output;
+    })
+    
+    const showBtn = document.querySelector('.existing-users');
+    showBtn.addEventListener('click', showCards);
+
+    function showCards(){
+      document.querySelector('.users').style.opacity = ('1')
+    }
