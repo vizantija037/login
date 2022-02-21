471a16fe0a89dcbfe806ff055b795c10388be28f
vizantija037 <80783335+vizantija037@users.noreply.github.com>
2022-02-21 12:24:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,83 +7,73 @@ function submitForm(e) {
   let userFront = document.getElementById("username").value;
   let passFront = document.getElementById("password").value;
   loaderVisible();
-  functionAsync()
-  async function functionAsync(){
-  try {
-    await fetch("https://fakestoreapi.com/users")
-    .then((resp) => resp.json())
-    .then((data) => {
- 
-      for (let i = 0; i < data.length; i++) {
-        if (data[i].username !== userFront && data[i].password !== passFront) {
-          document.querySelector(".user-message").style.display = "flex";
-          
-          
-        } else {
-          NewTab();
-          form.reset();
-          document.querySelector(".user-message").style.display = "none";
-        }
-        
-        
-      }
-    })
-  }
-  catch(err){
-    console.log(err)
-  }
-  finally{
-    loaderInvisible();
-  }
-  }
- 
-   
-}
+  functionAsync();
+  async function functionAsync() {
+    try {
+      await fetch("https://fakestoreapi.com/users")
+        .then((resp) => resp.json())
+        .then((data) => {
+          for (let i = 0; i < data.length; i++) {
+            if (
+              data[i].username !== userFront &&
+              data[i].password !== passFront
+            ) {
+              document.querySelector(".user-message").style.display = "flex";
+            } else {
+              NewTab();
+              form.reset();
+              document.querySelector(".user-message").style.display = "none";
+              return;
+            }
 
+          }
+        });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      loaderInvisible();
+    }
+  }
+}
+//open new window
 function NewTab() {
-  window.open("https://vizantija037.github.io/JSON-reading/", "_self");
+  window.open("https://vizantija037.github.io/JSON-reading/", "_blank");
 }
-
-form.addEventListener('click', resetLabel);
-function resetLabel(e){
-  if (e.target.type === 'text'){
+//reset label - to inone
+form.addEventListener("click", resetLabel);
+function resetLabel(e) {
+  if (e.target.type === "text") {
     document.querySelector(".user-message").style.display = "none";
   }
 }
-
-const loader = document.querySelector('.loader-holder');
-
-function loaderVisible(){
-    loader.style.display = 'flex';
+//loader
+const loader = document.querySelector(".loader-holder");
+function loaderVisible() {
+  loader.style.display = "flex";
 }
 function loaderInvisible() {
-  loader.style.display = 'none';
+  loader.style.display = "none";
 }
 
-
-
-
-fetch('https://fakestoreapi.com/users')
-    .then(response=>response.json())
-    .then(data=>{
-        
-        let output = '';
-        data.forEach(element => {
-                 
-            output += `
+fetch("https://fakestoreapi.com/users")
+  .then((response) => response.json())
+  .then((data) => {
+    let output = "";
+    data.forEach((element) => {
+      output += `
             <div class="card">
             <h4>Username: ${element.username}</h4>
             <h4>Password: ${element.password}</h4>
             </div>
-        `}
-        ) 
-          
-        document.querySelector('.users').innerHTML = output;
-    })
-    
-    const showBtn = document.querySelector('.existing-users');
-    showBtn.addEventListener('click', showCards);
+        `;
+    });
 
-    function showCards(){
-      document.querySelector('.users').style.opacity = ('1')
-    }
+    document.querySelector(".users").innerHTML = output;
+  });
+
+const showBtn = document.querySelector(".existing-users");
+showBtn.addEventListener("click", showCards);
+
+function showCards() {
+  document.querySelector(".users").style.opacity = "1";
+}
